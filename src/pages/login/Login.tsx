c878258635677317c80708f13cc32e662c54d261
Abhijit Sonar <abhijit.sonar@thinkitive.com>
2023-07-04 19:04:19 +0530

    Re-write login form using mantine

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,54 +1,96 @@
-import { Field, Form, Formik } from "formik";
 import { login } from "../../api";
 import { Link, useNavigate } from "react-router-dom";
-import { useState } from "react";
+import {
+  Button,
+  Checkbox,
+  Container,
+  Flex,
+  Space,
+  Text,
+  TextInput,
+  Title,
+  rem,
+} from "@mantine/core";
+import { useForm } from "@mantine/form";
 
 type LoginFormData = {
   email: string;
   password: string;
+  termsAccepted: boolean;
 };
 
 export default function Login() {
   const navigate = useNavigate();
 
-  const [termsAccepted, setTermsAccepted] = useState(false);
+  const form = useForm<LoginFormData>({
+    initialValues: {
+      email: "",
+      password: "",
+      termsAccepted: false,
+    },
+    validate: {
+      email: (value) => (/^\S+@\S+$/.test(value) ? null : "Invalid email"),
+      password: (value) =>
+        value.length > 0 ? null : "Please enter your password",
+    },
+  });
 
   return (
-    <div>
-      <h2>Login</h2>
-      <Formik
-        initialValues={{ email: "", password: "" }}
-        onSubmit={(form: LoginFormData) => {
-          login(form.email, form.password).then(() => {
-            navigate("/");
-          });
-        }}
+    <Container my="lg" pt={rem(64)}>
+      <Title ta="center" order={1}>
+        Login
+      </Title>
+
+      <Space h="lg" />
+
+      <form
+        onSubmit={form.onSubmit((values) => {
+          login(values.email, values.password).then(() => navigate("/"));
+        })}
       >
-        <Form>
-          <Field name="email" type="email" placeholder="e-mail"></Field>
-          <Field name="password" type="password" placeholder="Password"></Field>
+        <Flex direction="column" gap="sm">
+          <TextInput
+            name="email"
+            type="email"
+            placeholder="you@example.com"
+            label="Your email"
+            {...form.getInputProps("email")}
+          ></TextInput>
 
-          <div>
-            <input
-              type="checkbox"
+          <TextInput
+            name="password"
+            type="password"
+            placeholder="Password"
+            label="Your password"
+            {...form.getInputProps("password")}
+          ></TextInput>
+
+          <Flex align="center" gap="sm">
+            <Checkbox
+              name="termsAccepted"
               id="checkbox-terms"
-              checked={termsAccepted}
-              onChange={(e) => setTermsAccepted(e.target.checked)}
+              {...form.getInputProps("termsAccepted")}
             />
-            <label htmlFor="checkbox-terms">
-              I agree to the terms of service
-            </label>
-          </div>
 
-          <button type="submit" disabled={!termsAccepted}>
+            <Text component="label" htmlFor="checkbox-terms">
+              I consent to selling of my data
+            </Text>
+          </Flex>
+
+          <Button
+            mt="lg"
+            type="submit"
+            size="lg"
+            disabled={!form.values.termsAccepted}
+          >
             Login
-          </button>
-        </Form>
-      </Formik>
+          </Button>
+        </Flex>
+      </form>
 
-      <p>
+      <Text mt="lg">
         Don't have an account? <Link to={"/register"}>Create one</Link>
-      </p>
-    </div>
+      </Text>
+    </Container>
   );
 }
